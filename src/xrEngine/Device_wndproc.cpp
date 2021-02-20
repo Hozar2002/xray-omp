7879dc6c8fa1f82172bffcfa086c7c83f784257f
Hozar2002 <hoz2002team@gmail.com>
2021-02-20 19:17:37 +0500

    fix closing game from taskbar menu

--- src/xrEngine/Device_wndproc.cpp
+++ src/xrEngine/Device_wndproc.cpp
@@ -43,11 +43,12 @@ bool CRenderDevice::on_message	(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lPar
 			return			(false);
 		}
 		case WM_CLOSE : {
+			Engine.Event.Defer("KERNEL:disconnect");
+			Engine.Event.Defer("KERNEL:quit");
 #ifdef INGAME_EDITOR
 			if (editor())
 				break;
 #endif // #ifdef INGAME_EDITOR
-
 			result			= 0;
 			return			(true);
 		}
